a2052907cc2b026a0daa804303d60252fb169edd
Jason Dullack <jdullack@gmail.com>
2026-01-21 18:31:37 -0800

    x

--- src/Main.js
+++ src/Main.js
@@ -5,7 +5,7 @@ return (
 <div className="main">
         <div className="row row-2">
                 <div className="text-panel text-panel-alpenrose" id="alpenrose">
-                        <h1 className="section-title-alpenrose">Oy Retreats </h1>
+                        <h1 className="section-title-alpenrose">He Retreats </h1>
                         <p className="section-text-alpenrose">Description of the overall company goes here. Step into a space designed for pause, perspective, and possibility. This is an invitation to step away from the noise and reconnect with what matters most. Nothing here is rushed, prescribed, or performative. The experience begins the moment you arrive.</p>
                         <button className="learn-more-button learn-more-button-alpenrose"><p className="learn-more">Learn More</p></button>
                 </div>
